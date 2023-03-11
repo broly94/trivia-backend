61e1e625bf5dd5c0b2bbb1af67b534684e8f87cf
joaquin carro <leonel.carro94@gmail.com>
2023-03-11 03:00:20 -0300

    test production render & alwaysdata

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,21 +1,21 @@
 const config = {
     init: {
-        host: process.env.HOST || 'localhost',
-        port: process.env.PORT || 3002,
+        host: process.env.HOST,
+        port: process.env.PORT,
     },
     database: {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 3306,
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || 'root',
-        dataBaseName: process.env.DB_NAME || 'trivia',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        dataBaseName: process.env.DB_NAME,
     },
     jwt: {
-        jwtSecret: process.env.JWT_SECRET || 'triviaapp@',
-        jwtSecretResetPassword: process.env.JWT_SECRET_RESET_PASSWORD || 'triviaapp@123',
+        jwtSecret: process.env.JWT_SECRET || 'secret',
+        jwtSecretResetPassword: process.env.JWT_SECRET_RESET_PASSWORD || 'secretResetPassword',
     },
     mail: {
-        passwordGmail: process.env.PASSWORD_GMAIL || 'keozfdhewyyyjqxr'
+        passwordGmail: process.env.PASSWORD_GMAIL
     }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv'
+if(process.env.NODE_ENV !== 'production'){
+    dotenv.config()
+}
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
@@ -21,37 +25,29 @@ class ServerBootstrap {
         this.app.use(express.json())
         this.app.use(express.urlencoded({ extended: true }))
         this.app.use(cors())
+        this.dbConntect()
         this.app.use('/api', this.routes())
         this.listen()
-        myDataSource
-            .initialize()
-            .then(() => {
-                console.log("Data Source has been initialized!")
-            })
-            .catch(() => {
-                console.log("Data Source has not been initialized!")
-            })
+       
     }
 
 
 
     listen() {
         this.app.listen(this.port, () => {
+            console.log(process.env.NODE_ENV)
             console.log(`Server listening on port ${this.port}`)
         })
     }
 
-    // protected async dbConntect() {
-    //     try {
-    //         const conn = await this.typeORMConfig().initialize()
-    //         if (!conn) {
-    //             throw new Error()
-    //         }
-    //         console.log("Db connection successful")
-    //     } catch (error) {
-    //         console.log({ message: `No se pudo conecar a la DB: ${error}` })
-    //     }
-    // }    
+    protected async dbConntect() {
+        try {
+            await myDataSource.initialize()
+            console.log("Db connection successful")
+        } catch (error) {
+            console.log({ message: `No se pudo conecar a la DB: ${error}` })
+        }
+    }    
 
     private routes(): Array<express.Router> {
         return [
